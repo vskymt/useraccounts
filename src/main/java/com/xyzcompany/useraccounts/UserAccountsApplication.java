5804db1f9146a137b8a542f27741548790ff3edf
Vamshi Krishna <vskymt@gmail.com>
2024-12-26 15:17:37 +0530

    Initial commit

---
+++ src/main/java/com/xyzcompany/useraccounts/UserAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.xyzcompany.useraccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAccountsApplication.class, args);
+	}
+
+}
